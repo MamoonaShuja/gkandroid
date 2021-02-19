191c5c7c49c8b038de5d32ec9296d46d2e5bfff9
Haider Saleem Qamar <haidersaleemqamar@localhost.localdomain>
2021-02-19 16:56:13 +0500

    New PROJECT

--- app/src/main/java/com/example/gkrozgar/MainActivity.java
+++ app/src/main/java/com/example/gkrozgar/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.gkrozgar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
+import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),SignInPage.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
 }
